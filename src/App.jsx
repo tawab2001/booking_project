61437737a16f08148e2410fe2679d3e14051853f
yasorefaai1245 <yasorefaai1245@gmail.com>
2025-04-20 16:40:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+// import Applayout from "./components/Applayout";
 import Home from './pages/Home/home';
-import Logein from './pages/Logein/logein';
+import Logein from './pages/Logein/logein'; 
 import Profile from './pages/Profile/profile';
 import SignUp from './pages/signUp/signUp';
 import Events from './pages/Events/events';
 
 const router = createBrowserRouter([
   {
-    path: "",
-    // element: <Applayout/>,
+    path: "/",
+    element: <Applayout />, // استدعينا Applayout هنا
     children: [
-      { path: "", element: <Home/> },
-      { path: "Logein", element: <Logein/> },
-      { path: "Profile", element: <Profile/> },
-      {path: "SignUp", element:<SignUp/>},
-      {path: "Eventsdetils", element:< Events/>}
+      { path: "", element: <Home /> },
+      { path: "login", element: <Logein /> },
+      { path: "profile", element: <Profile /> },
+      { path: "signup", element: <SignUp /> },
+      { path: "eventdetails", element: <Events /> },
     ],
   },
 ]);
+
 function App() {
- 
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+function Header() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home" className="text-warning fw-bold">EasyTick</Navbar.Brand>
+
+        <Nav className="mx-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Host Events</Nav.Link>
+          <Nav.Link href="#pricing">Events</Nav.Link>
+          <Nav.Link href="#profile">Profile</Nav.Link>
+          <Nav.Link href="#contact">Contact Us</Nav.Link>
+          <Button variant="outline-light" size="sm">EN</Button>
+        </Nav>
+
+        <div className="d-flex gap-2 align-items-center">
+          <Button variant="warning">Login</Button>
+          <Button variant="outline-warning">Sign up</Button>
+        </div>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
